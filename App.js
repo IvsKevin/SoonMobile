96a73202bd0f5fd4fd35fdc0cd55940185a51740
Kevin Perdomo <kevinperdomo.ag@gmail.com>
2024-06-04 09:02:46 -0700

    Update App

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-// Depedencies
+// Depedencias
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// Components
+// Componentes
 import Input from './components/Input';
 import Button from './components/Button';
 import HorizontalLine from './components/HorizontalLine';
-import NavigationButton from './components/NavigationButton'; 
+import NavigationLink from './components/NavigationLink';
+
+// Pantallas
+import Home from './screens/HomeScreen';
+import Register from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [email, setEmail] = useState('');
@@ -16,21 +23,26 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text style={styles.title}>¡Bienvenido a Soon!</Text>
-        <View style={styles.containerText}>
-          <Text style={styles.titleText}>Inicia sesión o registrate</Text>
-          <Text style={styles.text}>Ingresa tu correo electrónico y contraseña</Text>
-        </View>
-        <Input placeholder="Ingresa tu email" value={email} onChange={setEmail} />
-        <Input placeholder="Ingresa tu contraseña" value={password} onChange={setPassword} />
-        <Button text="Iniciar sesión" onPress={() => console.log("Botón presionado")} backgroundColor="black"
-          color="#fff"
-        />
-        <HorizontalLine color="black" thickness={2} />
-
-        <Text style={styles.text}>¿No tienes una cuenta? <NavigationButton title="Registrate" destination="RegisterScreen"/></Text>
-      </View>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" options={{ headerShown: false }}>
+          {() => (
+            <View style={styles.container}>
+              <Text style={styles.title}>¡Bienvenido a Soon!</Text>
+              <View style={styles.containerText}>
+                <Text style={styles.titleText}>Inicia sesión o regístrate</Text>
+                <Text style={styles.text}>Ingresa tu correo electrónico y contraseña</Text>
+              </View>
+              <Input placeholder="Ingresa tu email" value={email} onChange={setEmail} />
+              <Input placeholder="Ingresa tu contraseña" value={password} onChange={setPassword} />
+              <Button text="Iniciar sesión" destination="Home" backgroundColor="black" color="#fff"/>
+              <HorizontalLine color="black" thickness={2} />
+              <Text style={styles.text}>¿No tienes una cuenta? <NavigationLink title="Regístrate" destination="Register" /></Text>
+            </View>
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
@@ -54,7 +66,7 @@ const styles = StyleSheet.create({
   },
   titleText: {
     fontSize: 20,
-    fontWeight: 'semi-bold',
+    fontWeight: '600',
     marginBottom: 10,
   },
   text: {
